838c12926bb18b188c3738843cbc53c3104bff30
Vasya1988 <myhappy1988@gmail.com>
2023-11-17 15:51:33 +0300

    upd tv shows page

--- app/tvshows/page.tsx
+++ app/tvshows/page.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+'use client'
+import React, { useEffect, useState } from "react";
+import MovieCard from "../components/movieCard/MovieCard";
+import { KinopoiskApiGenre } from "../api/Kinopoisk";
+import Styles from './Tvshows.module.sass';
+import { useGlobalContext } from "../Context/Context";
 
 const Tvshows = () => {
+    const {genreName} = useGlobalContext()
+    const [apiTvShow, setTvShow] = useState([]);
+    useEffect(() => {
+        (
+            async ()=>{
+                const responseApi = await KinopoiskApiGenre(genreName, '', 'tv-series')
+                const getTvShow = await responseApi;
+                await setTvShow(getTvShow.docs)
+            }
+        )()
+    }, [genreName])
+    console.log(apiTvShow)
     return (
-        <main>
-            Hello, from TV Shows
+        <main className={Styles.TvFrame}>
+
+            {
+                apiTvShow && apiTvShow.map((name, index) => <MovieCard key={index} movieApi={name} />)
+            }
+
         </main>
     )
 };
