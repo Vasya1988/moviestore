2cc77b4ca8c8d12bbf8d19e1b317985e2d063dff
Vasily Romanov <va5ily@MacBook-Air-Vasily.local>
2023-12-25 22:06:46 +0300

    upd open card

--- app/components/openCard/OpenCard.tsx
+++ app/components/openCard/OpenCard.tsx
@@ -1,5 +1,7 @@
 import Title from '../title/Title';
 import Styles from './OpenCard.module.sass'
+import CloseButton from '../buttons/close/Close';
+import { DeleteCard } from './OpenCardHelper';
 
 interface movieProps {
     name?: string,
@@ -18,7 +20,7 @@ const OpenCard = ({name, imageLInk, description, year, countries}:movieProps) =>
                     <span>{`${year}, ${countries}`}</span>
                     <span className={Styles.Description} >{description}</span>
                 </div>
-
+                <CloseButton func={(event:any) => DeleteCard(event)} color={'var(--ButtonColor)'} />
             </div>
         </div>
     )
---
+++ app/components/openCard/OpenCardHelper.ts
@@ -0,0 +1,7 @@
+interface DeleteProps {
+    eve: any
+}
+
+export const DeleteCard = ({eve}: DeleteProps) => {
+    console.log(eve)
+}
