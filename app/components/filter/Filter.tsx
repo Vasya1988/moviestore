71c0d9fd73e1457a3a35e4132504aa23d41d0320
Vasya1988 <myhappy1988@gmail.com>
2024-02-27 17:02:57 +0300

    update mobile css

--- app/components/filter/Filter.tsx
+++ app/components/filter/Filter.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Styles from './Filter.module.sass';
 import { FilterList } from './FilterHelper';
 import { useGlobalContext } from '@/app/Context/Context';
@@ -7,16 +7,32 @@ import { useGlobalContext } from '@/app/Context/Context';
 
 const Filter = (props: any) => {
     const {genreName, setGenreName} = useGlobalContext()
+    const [showGenre, setShowGenre] = useState('none')
     
     return (
         <nav className={Styles.Filter}>
-            <h2 className={Styles.GenreMobile}> Choose genre</h2>
-            <ul>
+            <h2 
+                className={Styles.GenreMobile}
+                onClick={
+                    () => {
+                        showGenre === 'none' 
+                            ? setShowGenre('block')
+                            : setShowGenre('none')
+                    }
+                }
+            >
+                Choose genre
+            </h2>
+            <ul style={{display: showGenre}}>
                 { FilterList.map((name, index) => 
                     <li 
                         className={name.name === props.active ? 'active' : ''}
                         key={index} 
-                        onClick={(event) => {setGenreName(event.currentTarget.dataset.genre)}} 
+                        onClick={(event) => {
+                            setGenreName(event.currentTarget.dataset.genre); 
+                            setShowGenre('none'); 
+                            window.scrollTo(0,0)}
+                        } 
                         data-genre={name.genre}>
                             <a>{name.name}</a>
                     </li>
